9e2e4687f5cc1b26fa3f5a69a8348024a6c7c5af
tmrekk121 <tmrekk121@outlook.jp>
2019-09-21 22:22:44 +0900

    add line login process

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top_pages/home'
+  get 'top_pages/line_login'
 end
