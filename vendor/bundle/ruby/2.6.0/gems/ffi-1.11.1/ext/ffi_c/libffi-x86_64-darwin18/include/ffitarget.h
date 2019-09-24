1abbe5c2a15ff8b63d854acd1152162d044fbf74
tmrekk121 <tmrekk121@outlook.jp>
2019-09-24 22:40:47 +0900

    fix the ability to deploy to heroku

--- vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/e175754/DONE/DONE/vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/bundle/ruby/2.6.0/gems/parser-2.6.4.1/lib/parser/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Parser
-  VERSION = '2.6.4.1'
-end
