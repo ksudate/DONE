743e7fe4ef57921ebbc9435bc48ab9f13c3c4961
tmrekk121 <tmrekk121@outlook.jp>
2019-09-25 01:32:32 +0900

    fix linebot process

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,8 +1,9 @@
 class LineBotController < ApplicationController
   require 'line/bot'
+  protect_from_forgery :except => [:callback]
+
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_id = Rails.application.credentials.linebot[:channel_id]
       config.channel_secret = Rails.application.credentials.linebot[:channel_secret]
       config.channel_token = Rails.application.credentials.linebot[:channel_token]
     }
@@ -10,21 +11,23 @@ class LineBotController < ApplicationController
 
   def callback
     body = request.body.read
-    
+
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       head 400
     end
 
     events = client.parse_events_from(body)
+
     events.each do |event|
+      response = '今日のタスクは' + "\n"
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: event.message['text']
+            text: response
           }
           client.reply_message(event['replyToken'], message)
         end
