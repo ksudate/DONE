1f97b8bedec41d42148b345b34b11d83fef820e6
tmrekk121 <tmrekk121@outlook.jp>
2019-10-26 23:55:57 +0900

    modify splint form

--- app/controllers/splints_controller.rb
+++ app/controllers/splints_controller.rb
@@ -2,57 +2,61 @@ require 'net/http'
 require 'json'
 
 class SplintsController < ApplicationController
-    include Common
-    before_action :line_login, only: [:index]
+  include Common
+  before_action :line_login, only: [:index]
 
-    def index
-      @keep = Splint.where(line_id: session[:line_id], kpt: 'Keep')
-      @problem = Splint.where(line_id: session[:line_id], kpt: 'Problem')
-      @try = Splint.where(line_id: session[:line_id], kpt: 'Try')
-    end
+  def index
+    @keep = Splint.where(line_id: session[:line_id], kpt: 'Keep')
+    @problem = Splint.where(line_id: session[:line_id], kpt: 'Problem')
+    @try = Splint.where(line_id: session[:line_id], kpt: 'Try')
+  end
 
-    def show
-      @splint = Splint.find(params[:id])
-    end
-    
-    def new
-      @splint = Splint.new
-      @sp_number = params[:sp_number]
-    end
+  def show
+    @splint = Splint.find(params[:id])
+  end
+  
+  def new
+    @splint = Splint.new
+    @sp_number = params[:sp_number]
+  end
 
-    def edit
-      @splint = Splint.find(params[:id])
-    end
+  def edit
+    @splint = Splint.find(params[:id])
+  end
 
-    def create
-      if session[:access_token].nil?
-        render action: :index
-        flash.now[:danger] = 'ログインしてください。'
-      else
-        Splint.create(splint_params)
-        redirect_to splints_path
-      end
-    end
-    
-    def update
-      splint = Splint.find(params[:id])
-      splint.update(splint_edit_params)
+  def create
+    if session[:access_token].nil?
+      render action: :index
+      flash.now[:danger] = 'ログインしてください。'
+    else
+      Splint.create(splint_params)
       redirect_to splints_path
     end
+  end
+  
+  def update
+    splint = Splint.find(params[:id])
+    splint.update(splint_edit_params)
+    redirect_to splints_path
+  end
 
-    def destroy
-      @splint = Splint.find(params[:id])
-      @splint.destroy
-      redirect_to splints_path
-    end
+  def destroy
+    @splint = Splint.find(params[:id])
+    @splint.destroy
+    redirect_to splints_path
+  end
 
-    private
-    
-    def splint_params
-      params.require(:splint).permit(:content, :line_id, :kpt, :sp_number)
-    end
+  def destroy_all
+    Splint.where(sp_number: params[:sp_number]).destroy_all
+  end
 
-    def splint_edit_params
-      params.require(:splint).permit(:content, :line_id)
-    end
+  private
+  
+  def splint_params
+    params.require(:splint).permit(:content, :line_id, :kpt, :sp_number)
+  end
+
+  def splint_edit_params
+    params.require(:splint).permit(:content, :line_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'top_pages#home'
-  get '/logout', to: 'top_pages#logout'
+  get 'logout', to: 'top_pages#logout'
   post 'callback', to: 'line_bot#callback'
   resources :posts
   resources :splints
