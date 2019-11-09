bcaacb6a35200bf502e2e2f0bfbe2d4f78fbd7eb
tmrekk121 <tmrekk121@outlook.jp>
2019-11-09 17:17:35 +0900

    Convert web server to puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,16 @@
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
+
 plugin :tmp_restart
+
