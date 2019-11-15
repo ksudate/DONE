d108ae3f9e01c5a1f974a616e416638bf6bc3803
tmrekk121 <tmrekk121@outlook.jp>
2019-11-15 15:20:35 +0900

    Modify flash message after login

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -37,6 +37,7 @@ class LineBotController < ApplicationController
     head :bad_request unless client.validate_signature(body, signature)
     events = client.parse_events_from(body)
     events.each do |event|
+      logger.debug(event)
       response = fetch_task(event)
       case event
       when Line::Bot::Event::Message
