f9a8560887b1c4a263bb2e0bc6940eca682048f5
tmrekk121 <tmrekk121@outlook.jp>
2019-11-26 17:43:57 +0900

    Fix Model Validate

--- app/models/splint.rb
+++ app/models/splint.rb
@@ -1,9 +1,5 @@
 class Splint < ApplicationRecord
-<<<<<<< HEAD
   # belongs_to :user
-=======
-  belongs_to :user
->>>>>>> b3d6c7a24d368f7b90b93dba8663a9bfedef9579
   validates :line_id, presence: true
   validates :content, presence: true, length: { maximum: 60 }
 end
