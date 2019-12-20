8dc2ceadcf4bb830258a342bf154a149695adbcd
tmrekk121 <tmrekk121@outlook.jp>
2019-12-20 23:26:05 +0900

    Delete  Unnecessary comments

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -3,17 +3,17 @@ class TopPagesController < ApplicationController
   before_action :setup, only: [:home]
 
   def home
-    @url = 'https://access.line.me/oauth2/v2.1/authorize' \
-         + '?response_type=code&client_id=' + @client_id.to_s \
-         + '&redirect_uri=http%3a%2f%2flocalhost%3a3000%2fposts' \
-         + '&state=12345abcde' \
-         + '&bot_prompt=aggressive' \
-         + '&scope=openid%20profile' \
     # @url = 'https://access.line.me/oauth2/v2.1/authorize' \
     #      + '?response_type=code&client_id=' + @client_id.to_s \
-    #      + '&redirect_uri=https%3a%2f%2ftmrekk121%2ddone%2eherokuapp%2ecom%2fposts' \
+    #      + '&redirect_uri=http%3a%2f%2flocalhost%3a3000%2fposts' \
     #      + '&state=12345abcde' \
     #      + '&bot_prompt=aggressive' \
     #      + '&scope=openid%20profile' \
+    @url = 'https://access.line.me/oauth2/v2.1/authorize' \
+         + '?response_type=code&client_id=' + @client_id.to_s \
+         + '&redirect_uri=https%3a%2f%2ftmrekk121%2ddone%2eherokuapp%2ecom%2fposts' \
+         + '&state=12345abcde' \
+         + '&bot_prompt=aggressive' \
+         + '&scope=openid%20profile' \
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
-window.test_1 = function test_1() {
-    console.log("OK");
-}
